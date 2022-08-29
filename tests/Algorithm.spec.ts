4a50ad8c78accf40473156d0e5982ecbc7308763
claretnnamocha <devclareo@gmail.com>
2022-08-30 00:49:49 +0100

    remove generated dataset for binary search

--- tests/Algorithm.spec.ts
+++ tests/Algorithm.spec.ts
@@ -2,23 +2,20 @@ import assert from "assert";
 import BinarySearch from "../Algorithms/BinarySearch";
 
 describe("BinarySearch", function () {
-  const range = (start: number, stop: number) => {
-    let ar = [];
-    for (let index = start; index <= stop; index++) {
-      ar.push(index);
-    }
-    return ar;
-  };
+  const dataset = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+    60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+  ];
 
   it("can return true if in array", () => {
-    const dataset = range(1, 1000000);
-    const found = BinarySearch(dataset, 19406);
+    const found = BinarySearch(dataset, 48);
     assert.strictEqual(found, true);
   });
 
   it("can return false if not in array", () => {
-    const dataset = range(1, 1000000);
-    const found = BinarySearch(dataset, 2000000);
+    const found = BinarySearch(dataset, 80);
     assert.strictEqual(found, false);
   });
 });
