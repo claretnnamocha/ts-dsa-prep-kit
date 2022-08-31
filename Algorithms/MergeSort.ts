cedc9c35d87b0eeff17c9626ea16a1d51d84bce3
claretnnamocha <devclareo@gmail.com>
2022-08-31 08:21:26 +0100

    Update MergeSort to specify order

--- Algorithms/MergeSort.ts
+++ Algorithms/MergeSort.ts
@@ -1,4 +1,8 @@
-const merge = (sortedArray1: Array<any>, sortedArray2: Array<any>) => {
+const merge = (
+  sortedArray1: Array<any>,
+  sortedArray2: Array<any>,
+  ascendingOrder: boolean
+) => {
   let counter1 = 0,
     counter2 = 0;
   let result = [];
@@ -7,7 +11,11 @@ const merge = (sortedArray1: Array<any>, sortedArray2: Array<any>) => {
     const element1 = sortedArray1[counter1];
     const element2 = sortedArray2[counter2];
 
-    if (element1 < element2) {
+    const condition = ascendingOrder
+      ? element1 < element2
+      : element1 > element2;
+
+    if (condition) {
       result.push(element1);
       counter1 += 1;
     } else {
@@ -23,14 +31,18 @@ const merge = (sortedArray1: Array<any>, sortedArray2: Array<any>) => {
   ];
 };
 
-export const MergeSort = (array: Array<any>): Array<any> => {
+export const MergeSort = (
+  array: Array<any>,
+  ascendingOrder: boolean = true
+): Array<any> => {
   if (array.length === 1) return array;
 
   const middleIndex = (array.length / 2) | 0;
 
   return merge(
-    MergeSort(array.slice(0, middleIndex)),
-    MergeSort(array.slice(middleIndex, array.length))
+    MergeSort(array.slice(0, middleIndex), ascendingOrder),
+    MergeSort(array.slice(middleIndex, array.length), ascendingOrder),
+    ascendingOrder
   );
 };
 export default MergeSort;
--- tests/Algorithm.spec.ts
+++ tests/Algorithm.spec.ts
@@ -21,21 +21,40 @@ describe("BinarySearch", function () {
   });
 });
 
-describe("MergeSort", function () {
-  it("can sort an unsorted array", () => {
-    const sorted = MergeSort([1, 3, 5, 10, 11, 12, 2, 4, 6, 7, 8, 9]);
+describe("Sorting Algorithms", () => {
+  const dataset = [1, 3, 5, 10, 11, 12, 2, 4, 6, 7, 8, 9];
+  const sortedDataset1ASC = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const sortedDataset1DESC = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
-    assert.deepStrictEqual(sorted, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
-  });
+  const dataset2 = [
+    1, 3, 5, 10, 11, 8, 5, 12, 2, 4, 6, 7, 6, 8, 9, 0, 7, 3, 6, 1,
+  ];
+  const sortedDataset2ASC = [
+    0, 1, 1, 2, 3, 3, 4, 5, 5, 6, 6, 6, 7, 7, 8, 8, 9, 10, 11, 12,
+  ];
+  const sortedDataset2DESC = [
+    12, 11, 10, 9, 8, 8, 7, 7, 6, 6, 6, 5, 5, 4, 3, 3, 2, 1, 1, 0,
+  ];
 
-  it("can sort an unsorted array with duplicates", () => {
-    const sorted = MergeSort([
-      1, 3, 5, 10, 11, 8, 5, 12, 2, 4, 6, 7, 6, 8, 9, 0, 7, 3, 6, 1,
-    ]);
+  describe("MergeSort", function () {
+    it("can sort an unsorted array", () => {
+      const sorted = MergeSort(dataset);
+      assert.deepStrictEqual(sorted, sortedDataset1ASC);
+    });
 
-    assert.deepStrictEqual(
-      sorted,
-      [0, 1, 1, 2, 3, 3, 4, 5, 5, 6, 6, 6, 7, 7, 8, 8, 9, 10, 11, 12]
-    );
+    it("can sort an unsorted array in descending order", () => {
+      const sorted = MergeSort(dataset, false);
+      assert.deepStrictEqual(sorted, sortedDataset1DESC);
+    });
+
+    it("can sort an unsorted array with duplicates", () => {
+      const sorted = MergeSort(dataset2);
+      assert.deepStrictEqual(sorted, sortedDataset2ASC);
+    });
+
+    it("can sort an unsorted array with duplicates in descending order", () => {
+      const sorted = MergeSort(dataset2, false);
+      assert.deepStrictEqual(sorted, sortedDataset2DESC);
+    });
   });
 });
