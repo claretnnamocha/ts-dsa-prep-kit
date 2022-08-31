54ee9fbe92afcdbfb11f1fe7d0f8adf7c8d4bdbe
claretnnamocha <devclareo@gmail.com>
2022-08-31 22:26:01 +0100

    Group tests

--- tests/Algorithm.spec.ts
+++ tests/Algorithm.spec.ts
@@ -4,103 +4,107 @@ import BubbleSort from "../Algorithms/BubbleSort";
 import MergeSort from "../Algorithms/MergeSort";
 import SelectionSort from "../Algorithms/SelectionSort";
 
-describe("BinarySearch", function () {
-  const dataset = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
-    60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
-  ];
+describe("Algorithms", () => {
+  describe("Searching Algorithms", () => {
+    describe("BinarySearch", function () {
+      const dataset = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+        39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56,
+        57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+      ];
 
-  it("can return true if in array", () => {
-    const found = BinarySearch(dataset, 48);
-    assert.strictEqual(found, true);
-  });
+      it("can return true if in array", () => {
+        const found = BinarySearch(dataset, 48);
+        assert.strictEqual(found, true);
+      });
 
-  it("can return false if not in array", () => {
-    const found = BinarySearch(dataset, 80);
-    assert.strictEqual(found, false);
-  });
-});
-
-describe("Sorting Algorithms", () => {
-  const dataset = [1, 3, 5, 10, 11, 12, 2, 4, 6, 7, 8, 9];
-  const sortedDataset1ASC = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  const sortedDataset1DESC = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-
-  const dataset2 = [
-    1, 3, 5, 10, 11, 8, 5, 12, 2, 4, 6, 7, 6, 8, 9, 0, 7, 3, 6, 1,
-  ];
-  const sortedDataset2ASC = [
-    0, 1, 1, 2, 3, 3, 4, 5, 5, 6, 6, 6, 7, 7, 8, 8, 9, 10, 11, 12,
-  ];
-  const sortedDataset2DESC = [
-    12, 11, 10, 9, 8, 8, 7, 7, 6, 6, 6, 5, 5, 4, 3, 3, 2, 1, 1, 0,
-  ];
-
-  describe("MergeSort", function () {
-    it("can sort an unsorted array", () => {
-      const sorted = MergeSort(dataset);
-      assert.deepStrictEqual(sorted, sortedDataset1ASC);
-    });
-
-    it("can sort an unsorted array in descending order", () => {
-      const sorted = MergeSort(dataset, false);
-      assert.deepStrictEqual(sorted, sortedDataset1DESC);
-    });
-
-    it("can sort an unsorted array with duplicates", () => {
-      const sorted = MergeSort(dataset2);
-      assert.deepStrictEqual(sorted, sortedDataset2ASC);
-    });
-
-    it("can sort an unsorted array with duplicates in descending order", () => {
-      const sorted = MergeSort(dataset2, false);
-      assert.deepStrictEqual(sorted, sortedDataset2DESC);
+      it("can return false if not in array", () => {
+        const found = BinarySearch(dataset, 80);
+        assert.strictEqual(found, false);
+      });
     });
   });
 
-  describe("SelectionSort", function () {
-    it("can sort an unsorted array", () => {
-      const sorted = SelectionSort(dataset);
-      assert.deepStrictEqual(sorted, sortedDataset1ASC);
+  describe("Sorting Algorithms", () => {
+    const dataset = [1, 3, 5, 10, 11, 12, 2, 4, 6, 7, 8, 9];
+    const sortedDataset1ASC = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    const sortedDataset1DESC = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+    const dataset2 = [
+      1, 3, 5, 10, 11, 8, 5, 12, 2, 4, 6, 7, 6, 8, 9, 0, 7, 3, 6, 1,
+    ];
+    const sortedDataset2ASC = [
+      0, 1, 1, 2, 3, 3, 4, 5, 5, 6, 6, 6, 7, 7, 8, 8, 9, 10, 11, 12,
+    ];
+    const sortedDataset2DESC = [
+      12, 11, 10, 9, 8, 8, 7, 7, 6, 6, 6, 5, 5, 4, 3, 3, 2, 1, 1, 0,
+    ];
+
+    describe("MergeSort", function () {
+      it("can sort an unsorted array", () => {
+        const sorted = MergeSort(dataset);
+        assert.deepStrictEqual(sorted, sortedDataset1ASC);
+      });
+
+      it("can sort an unsorted array in descending order", () => {
+        const sorted = MergeSort(dataset, false);
+        assert.deepStrictEqual(sorted, sortedDataset1DESC);
+      });
+
+      it("can sort an unsorted array with duplicates", () => {
+        const sorted = MergeSort(dataset2);
+        assert.deepStrictEqual(sorted, sortedDataset2ASC);
+      });
+
+      it("can sort an unsorted array with duplicates in descending order", () => {
+        const sorted = MergeSort(dataset2, false);
+        assert.deepStrictEqual(sorted, sortedDataset2DESC);
+      });
     });
 
-    it("can sort an unsorted array in descending order", () => {
-      const sorted = SelectionSort(dataset, false);
-      assert.deepStrictEqual(sorted, sortedDataset1DESC);
+    describe("SelectionSort", function () {
+      it("can sort an unsorted array", () => {
+        const sorted = SelectionSort(dataset);
+        assert.deepStrictEqual(sorted, sortedDataset1ASC);
+      });
+
+      it("can sort an unsorted array in descending order", () => {
+        const sorted = SelectionSort(dataset, false);
+        assert.deepStrictEqual(sorted, sortedDataset1DESC);
+      });
+
+      it("can sort an unsorted array with duplicates", () => {
+        const sorted = SelectionSort(dataset2);
+        assert.deepStrictEqual(sorted, sortedDataset2ASC);
+      });
+
+      it("can sort an unsorted array with duplicates in descending order", () => {
+        const sorted = SelectionSort(dataset2, false);
+        assert.deepStrictEqual(sorted, sortedDataset2DESC);
+      });
     });
 
-    it("can sort an unsorted array with duplicates", () => {
-      const sorted = SelectionSort(dataset2);
-      assert.deepStrictEqual(sorted, sortedDataset2ASC);
-    });
+    describe("BubbleSort", function () {
+      it("can sort an unsorted array", () => {
+        const sorted = BubbleSort(dataset);
+        assert.deepStrictEqual(sorted, sortedDataset1ASC);
+      });
 
-    it("can sort an unsorted array with duplicates in descending order", () => {
-      const sorted = SelectionSort(dataset2, false);
-      assert.deepStrictEqual(sorted, sortedDataset2DESC);
-    });
-  });
+      it("can sort an unsorted array in descending order", () => {
+        const sorted = BubbleSort(dataset, false);
+        assert.deepStrictEqual(sorted, sortedDataset1DESC);
+      });
 
-  describe("BubbleSort", function () {
-    it("can sort an unsorted array", () => {
-      const sorted = BubbleSort(dataset);
-      assert.deepStrictEqual(sorted, sortedDataset1ASC);
-    });
+      it("can sort an unsorted array with duplicates", () => {
+        const sorted = BubbleSort(dataset2);
+        assert.deepStrictEqual(sorted, sortedDataset2ASC);
+      });
 
-    it("can sort an unsorted array in descending order", () => {
-      const sorted = BubbleSort(dataset, false);
-      assert.deepStrictEqual(sorted, sortedDataset1DESC);
-    });
-
-    it("can sort an unsorted array with duplicates", () => {
-      const sorted = BubbleSort(dataset2);
-      assert.deepStrictEqual(sorted, sortedDataset2ASC);
-    });
-
-    it("can sort an unsorted array with duplicates in descending order", () => {
-      const sorted = BubbleSort(dataset2, false);
-      assert.deepStrictEqual(sorted, sortedDataset2DESC);
+      it("can sort an unsorted array with duplicates in descending order", () => {
+        const sorted = BubbleSort(dataset2, false);
+        assert.deepStrictEqual(sorted, sortedDataset2DESC);
+      });
     });
   });
 });
--- tests/Data structures.spec.ts
+++ tests/Data structures.spec.ts
@@ -3,204 +3,206 @@ import LinkedList from "../Data Structures/LinkedList";
 import Queue from "../Data Structures/Queue";
 import Stack from "../Data Structures/Stack";
 
-describe("LinkedList", function () {
-  const list: LinkedList = new LinkedList();
-  beforeEach(() => {
-    list.clear();
+describe("Data structures", () => {
+  describe("LinkedList", function () {
+    const list: LinkedList = new LinkedList();
+    beforeEach(() => {
+      list.clear();
+    });
+
+    it("can add item", function () {
+      list.addItem(5);
+      assert.strictEqual(list.getHead().getValue(), 5);
+      assert.strictEqual(list.getTail().getValue(), 5);
+    });
+
+    it("can add item at head", function () {
+      list.addItem(10);
+      list.addItem(20);
+      list.addItem(30);
+      list.addItem(40);
+      list.addItem(50);
+      list.addItemAtHead(15);
+      assert.strictEqual(list.getHead().getValue(), 15);
+    });
+
+    it("can get index", function () {
+      list.addItem(10);
+      list.addItem(20);
+      list.addItem(30);
+      list.addItem(40);
+      list.addItem(50);
+      assert.strictEqual(list.getIndex(1).getValue(), 20);
+    });
+
+    it("can return null if index does not exist", function () {
+      list.addItem(10);
+      list.addItem(20);
+      list.addItem(30);
+      list.addItem(40);
+      list.addItem(50);
+      assert.strictEqual(list.getIndex(100), null);
+    });
+
+    it("can change value of node at specific index", function () {
+      list.addItem(10);
+      list.addItem(20);
+      list.addItem(30);
+      list.addItem(40);
+      list.addItem(50);
+      list.setIndex(1, 15);
+      assert.strictEqual(list.getIndex(1).getValue(), 15);
+      assert.strictEqual(list.getIndex(2).getValue(), 30);
+    });
+
+    it("can add item at index", function () {
+      list.addItem(10);
+      list.addItem(20);
+      list.addItem(30);
+      list.addItem(40);
+      list.addItem(50);
+      list.addItemAtIndex(1, 15);
+      assert.strictEqual(list.getIndex(1).getValue(), 15);
+      assert.strictEqual(list.getIndex(2).getValue(), 20);
+      assert.strictEqual(list.size(), 6);
+    });
+
+    it("can remove head", function () {
+      list.addItem(10);
+      list.addItem(20);
+      list.addItem(30);
+      list.addItem(40);
+      list.addItem(50);
+      list.removeHead();
+      assert.strictEqual(list.getHead().getValue(), 20);
+      assert.strictEqual(list.size(), 4);
+    });
+
+    it("can remove item at specific index", function () {
+      list.addItem(10);
+      list.addItem(20);
+      list.addItem(30);
+      list.addItem(40);
+      list.addItem(50);
+      list.addItem(60);
+      list.addItem(70);
+
+      assert.strictEqual(list.size(), 7);
+
+      list.removeItemAtIndex(2);
+      list.removeItemAtIndex(2);
+
+      assert.strictEqual(list.size(), 5);
+
+      list.addItem(80);
+      list.addItem(90);
+
+      assert.strictEqual(list.size(), 7);
+
+      list.removeItemAtIndex(0);
+      list.removeItemAtIndex(5);
+
+      assert.strictEqual(list.size(), 5);
+
+      assert.strictEqual(list.getHead().getValue(), 20);
+
+      assert.strictEqual(list.getTail().getValue(), 80);
+
+      list.removeItemAtIndex(0);
+      list.removeItemAtIndex(1);
+      list.removeItemAtIndex(2);
+      list.removeItemAtIndex(1);
+      list.removeItemAtIndex(0);
+
+      assert.strictEqual(list.size(), 0);
+    });
+
+    it("can remove tail", function () {
+      list.addItem(10);
+      list.addItem(20);
+      list.addItem(30);
+      list.addItem(40);
+      list.removeTail();
+      assert.strictEqual(list.getTail().getValue(), 30);
+    });
+
+    it("can clear list", function () {
+      list.addItem(10);
+      list.addItem(20);
+      list.addItem(30);
+      list.addItem(40);
+      list.clear();
+      assert.strictEqual(list.getHead(), null);
+      assert.strictEqual(list.getTail(), null);
+      assert.strictEqual(list.size(), 0);
+    });
   });
 
-  it("can add item", function () {
-    list.addItem(5);
-    assert.strictEqual(list.getHead().getValue(), 5);
-    assert.strictEqual(list.getTail().getValue(), 5);
+  describe("Queue", function () {
+    let queue: Queue = new Queue();
+    beforeEach(() => {
+      queue = new Queue();
+    });
+
+    it("can add item", function () {
+      queue.enqueue(10);
+      assert.strictEqual(queue.enqueue(10), 10);
+    });
+
+    it("can remove item", function () {
+      queue.enqueue(10);
+      queue.enqueue(20);
+      queue.enqueue(30);
+      assert.strictEqual(queue.dequeue(), 10);
+    });
+
+    it("can get index", function () {
+      queue.enqueue(10);
+      queue.enqueue(20);
+      queue.enqueue(30);
+      assert.strictEqual(queue.get(1), 20);
+    });
+
+    it("can check size", function () {
+      queue.enqueue(10);
+      queue.enqueue(20);
+      queue.enqueue(30);
+      queue.enqueue(40);
+      assert.strictEqual(queue.size(), 4);
+    });
   });
 
-  it("can add item at head", function () {
-    list.addItem(10);
-    list.addItem(20);
-    list.addItem(30);
-    list.addItem(40);
-    list.addItem(50);
-    list.addItemAtHead(15);
-    assert.strictEqual(list.getHead().getValue(), 15);
-  });
+  describe("Stack", function () {
+    let stack: Stack;
 
-  it("can get index", function () {
-    list.addItem(10);
-    list.addItem(20);
-    list.addItem(30);
-    list.addItem(40);
-    list.addItem(50);
-    assert.strictEqual(list.getIndex(1).getValue(), 20);
-  });
+    beforeEach(() => {
+      stack = new Stack();
+    });
 
-  it("can return null if index does not exist", function () {
-    list.addItem(10);
-    list.addItem(20);
-    list.addItem(30);
-    list.addItem(40);
-    list.addItem(50);
-    assert.strictEqual(list.getIndex(100), null);
-  });
+    it("can add item", function () {
+      assert.strictEqual(stack.push(10), 10);
+    });
 
-  it("can change value of node at specific index", function () {
-    list.addItem(10);
-    list.addItem(20);
-    list.addItem(30);
-    list.addItem(40);
-    list.addItem(50);
-    list.setIndex(1, 15);
-    assert.strictEqual(list.getIndex(1).getValue(), 15);
-    assert.strictEqual(list.getIndex(2).getValue(), 30);
-  });
+    it("can remove item", function () {
+      stack.push(10);
+      stack.push(20);
+      stack.push(30);
+      assert.strictEqual(stack.pop(), 30);
+    });
 
-  it("can add item at index", function () {
-    list.addItem(10);
-    list.addItem(20);
-    list.addItem(30);
-    list.addItem(40);
-    list.addItem(50);
-    list.addItemAtIndex(1, 15);
-    assert.strictEqual(list.getIndex(1).getValue(), 15);
-    assert.strictEqual(list.getIndex(2).getValue(), 20);
-    assert.strictEqual(list.size(), 6);
-  });
+    it("can get index", function () {
+      stack.push(10);
+      stack.push(20);
+      stack.push(30);
+      assert.strictEqual(stack.get(1), 20);
+    });
 
-  it("can remove head", function () {
-    list.addItem(10);
-    list.addItem(20);
-    list.addItem(30);
-    list.addItem(40);
-    list.addItem(50);
-    list.removeHead();
-    assert.strictEqual(list.getHead().getValue(), 20);
-    assert.strictEqual(list.size(), 4);
-  });
-
-  it("can remove item at specific index", function () {
-    list.addItem(10);
-    list.addItem(20);
-    list.addItem(30);
-    list.addItem(40);
-    list.addItem(50);
-    list.addItem(60);
-    list.addItem(70);
-
-    assert.strictEqual(list.size(), 7);
-
-    list.removeItemAtIndex(2);
-    list.removeItemAtIndex(2);
-
-    assert.strictEqual(list.size(), 5);
-
-    list.addItem(80);
-    list.addItem(90);
-
-    assert.strictEqual(list.size(), 7);
-
-    list.removeItemAtIndex(0);
-    list.removeItemAtIndex(5);
-
-    assert.strictEqual(list.size(), 5);
-
-    assert.strictEqual(list.getHead().getValue(), 20);
-
-    assert.strictEqual(list.getTail().getValue(), 80);
-
-    list.removeItemAtIndex(0);
-    list.removeItemAtIndex(1);
-    list.removeItemAtIndex(2);
-    list.removeItemAtIndex(1);
-    list.removeItemAtIndex(0);
-
-    assert.strictEqual(list.size(), 0);
-  });
-
-  it("can remove tail", function () {
-    list.addItem(10);
-    list.addItem(20);
-    list.addItem(30);
-    list.addItem(40);
-    list.removeTail();
-    assert.strictEqual(list.getTail().getValue(), 30);
-  });
-
-  it("can clear list", function () {
-    list.addItem(10);
-    list.addItem(20);
-    list.addItem(30);
-    list.addItem(40);
-    list.clear();
-    assert.strictEqual(list.getHead(), null);
-    assert.strictEqual(list.getTail(), null);
-    assert.strictEqual(list.size(), 0);
-  });
-});
-
-describe("Queue", function () {
-  let queue: Queue = new Queue();
-  beforeEach(() => {
-    queue = new Queue();
-  });
-
-  it("can add item", function () {
-    queue.enqueue(10);
-    assert.strictEqual(queue.enqueue(10), 10);
-  });
-
-  it("can remove item", function () {
-    queue.enqueue(10);
-    queue.enqueue(20);
-    queue.enqueue(30);
-    assert.strictEqual(queue.dequeue(), 10);
-  });
-
-  it("can get index", function () {
-    queue.enqueue(10);
-    queue.enqueue(20);
-    queue.enqueue(30);
-    assert.strictEqual(queue.get(1), 20);
-  });
-
-  it("can check size", function () {
-    queue.enqueue(10);
-    queue.enqueue(20);
-    queue.enqueue(30);
-    queue.enqueue(40);
-    assert.strictEqual(queue.size(), 4);
-  });
-});
-
-describe("Stack", function () {
-  let stack: Stack;
-
-  beforeEach(() => {
-    stack = new Stack();
-  });
-
-  it("can add item", function () {
-    assert.strictEqual(stack.push(10), 10);
-  });
-
-  it("can remove item", function () {
-    stack.push(10);
-    stack.push(20);
-    stack.push(30);
-    assert.strictEqual(stack.pop(), 30);
-  });
-
-  it("can get index", function () {
-    stack.push(10);
-    stack.push(20);
-    stack.push(30);
-    assert.strictEqual(stack.get(1), 20);
-  });
-
-  it("can check size", function () {
-    stack.push(10);
-    stack.push(20);
-    stack.push(30);
-    stack.push(40);
-    assert.strictEqual(stack.size(), 4);
+    it("can check size", function () {
+      stack.push(10);
+      stack.push(20);
+      stack.push(30);
+      stack.push(40);
+      assert.strictEqual(stack.size(), 4);
+    });
   });
 });
