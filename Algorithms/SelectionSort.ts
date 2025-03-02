cf692ec0618b34191de2e7aa1fa3c76857cf44fc
Claret Nnamocha <devclareo@gmail.com>
2025-03-02 09:36:34 +0100

    add TSDoc

--- Algorithms/SelectionSort.ts
+++ Algorithms/SelectionSort.ts
@@ -1,3 +1,10 @@
+/**
+ * Sorts an array using the selection sort algorithm.
+ *
+ * @param items - The array of items to sort.
+ * @param ascendingOrder - If true, sorts in ascending order; otherwise, sorts in descending order. Defaults to true.
+ * @returns The sorted array.
+ */
 export const SelectionSort = (
   items: Array<any>,
   ascendingOrder: boolean = true
