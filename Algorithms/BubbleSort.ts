03e0c49add7a3b2640ea6e204b4c848ad99da3a3
Claret Nnamocha <devclareo@gmail.com>
2025-03-02 09:01:03 +0100

    Update BubbleSort.ts

--- Algorithms/BubbleSort.ts
+++ Algorithms/BubbleSort.ts
@@ -1,27 +1,28 @@
-const moveToRight = (array: Array<any>, i: number) => {
-  const temp = array[i];
-  array[i] = array[i + 1];
-  array[i + 1] = temp;
-};
-
 export const BubbleSort = (
-  array: Array<any>,
+  items: Array<any>,
   ascendingOrder: boolean = true
 ): Array<any> => {
-  for (let iteration = 0; iteration < array.length; iteration++) {
-    for (let index = 0; index < array.length - iteration; index++) {
-      const element = array[index];
-      const nextElement = array[index + 1];
+  for (let iteration = 0; iteration < items.length - 1; iteration++) {
+    let swapped = false;
 
-      const condition = ascendingOrder
-        ? element > nextElement
-        : element < nextElement;
-
-      if (condition) moveToRight(array, index);
+    for (let index = 0; index < items.length - iteration - 1; index++) {
+      if (
+        (ascendingOrder && items[index] > items[index + 1]) ||
+        (!ascendingOrder && items[index] < items[index + 1])
+      ) {
+        // Swap elements
+        const temp = items[index];
+        items[index] = items[index + 1];
+        items[index + 1] = temp;
+        swapped = true;
+      }
     }
+
+    // If no swaps occurred, the array is sorted
+    if (!swapped) break;
   }
 
-  return array;
+  return items;
 };
 
 export default BubbleSort;
