8a5d1f0b0b0398aec4101d20bd471882082aa92e
Claret Nnamocha <devclareo@gmail.com>
2025-03-02 09:26:09 +0100

    Update BinarySearch.ts

--- Algorithms/BinarySearch.ts
+++ Algorithms/BinarySearch.ts
@@ -1,3 +1,10 @@
+/**
+ * Performs a binary search on a sorted array.
+ *
+ * @param sortedItems - A sorted array of items.
+ * @param item - The item to search for.
+ * @returns True if the item is found; otherwise, false.
+ */
 export const BinarySearch = (sortedItems: Array<any>, item: any): boolean => {
   let left = 0;
   let right = sortedItems.length - 1;
