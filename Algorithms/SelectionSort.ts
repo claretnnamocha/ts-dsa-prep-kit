e05d3b73e98525ae5380a2daaa2debb09c5f1136
Claret Nnamocha <devclareo@gmail.com>
2025-03-02 09:36:06 +0100

    Update SelectionSort.ts

--- Algorithms/SelectionSort.ts
+++ Algorithms/SelectionSort.ts
@@ -1,27 +1,26 @@
-const moveToLeft = (array: Array<any>, i: number) => {
-  const temp = array[i];
-  array[i] = array[i - 1];
-  array[i - 1] = temp;
-};
-
 export const SelectionSort = (
-  array: Array<any>,
+  items: Array<any>,
   ascendingOrder: boolean = true
 ): Array<any> => {
-  for (let iteration = 0; iteration < array.length; iteration++) {
-    for (let index = 0; index < array.length - iteration; index++) {
-      const element = array[index];
-      const nextElement = array[index + 1];
+  for (let i = 0; i < items.length - 1; i++) {
+    let minOrMaxIndex = i;
 
-      const condition = ascendingOrder
-        ? nextElement < element
-        : nextElement > element;
+    for (let j = i + 1; j < items.length; j++) {
+      const shouldSwap = ascendingOrder
+        ? items[j] < items[minOrMaxIndex]
+        : items[j] > items[minOrMaxIndex];
 
-      if (condition) moveToLeft(array, index + 1);
+      if (shouldSwap) minOrMaxIndex = j;
+    }
+
+    if (minOrMaxIndex !== i) {
+      const temp = items[i];
+      items[i] = items[minOrMaxIndex];
+      items[minOrMaxIndex] = temp;
     }
   }
 
-  return array;
+  return items;
 };
 
 export default SelectionSort;
